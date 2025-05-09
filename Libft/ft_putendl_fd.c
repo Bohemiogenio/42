3188d3534800b49760930d18a2bb369039e89eb4
Raul Maria Sanchez Hermosilla Zorrilla <raulsanc@c2r3s5.42malaga.com>
2025-05-09 15:37:38 +0200

    githud

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
 
--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
